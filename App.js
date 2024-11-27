805aa377fd7aaa8863683830fc67749dec0d7a64
vishnusharma1908 <vishnu2018.121@gmail.com>
2024-11-27 21:23:15 +0530

    Episode 3

--- App.js
+++ App.js
@@ -1,30 +1,58 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
- [
-    React.createElement("div", { id: "child1" }, [
-        React.createElement("h1", {}, "I am h1 tag"),
-        React.createElement("h2", {}, "I am h2 tag"),
-      ]),
-      React.createElement("div", { id: "child2" }, [
-        React.createElement("h1", {}, "I am h1 tag"),
-        React.createElement("h2", {}, "I am h2 tag"),
-      ])
- ]
-);
+/* 
+Just for Practice,Not Ideal Practice 
+*/
 
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello World from React!"
-);
+//React.createElement => ReactElement(JS Object) => HTMLElement(render)
 
-console.log(heading); //object
+//Code using proper react
+const heading = React.createElement("h1",{id:"heading"},"Episode 3");
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+//JSX  - is not HTML in JS (HTML or XML like syntax)
 
-root.render(parent);
-//root.render(heading);
+//JSX (transpiled before it reaches the JS Engine) - PARCEL - BABEL
+
+//JSX =>Babel transpiles it to React.createElement => ReactElement(JS Object) => HTMLElement(render)
+const jsxHeading =( 
+    <h1 id="heading" className="heading" tabIndex="1">
+    Jsx Heading Episode 3 
+    </h1>
+    );
+
+    //React Component
+    //1)class based components -Old way
+    //2)functional componets -new way
+
+    //React functional component
+    const Title = () => (
+        <h2>Heading under main Function</h2>
+    );
+
+    const elem = <span>React Element</span>;
+
+    //React Element
+    const elementheading = (
+        <h1 id="elementHeading">
+            {elem}
+            Element Heading</h1>
+    );
+
+    //React functional component
+    const HeadingComponent = () => (
+        <div id="container">
+         <h1>Function based component</h1>
+         <Title />
+         <Title> </Title>
+         {Title()}
+         <h3>{HeadingComponent.innerHTML="We can write any JS expression in this braces"}</h3>
+         {elementheading}
+         </div>
+    );
+
+    
+
+const root=ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<HeadingComponent />);
