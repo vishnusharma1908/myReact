da67b9faba208d7d9d786c884d00558f0d6d1201
vishnusharma1908 <vishnu2018.121@gmail.com>
2025-01-09 22:13:58 +0530

    episdoe 6

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,27 +1,66 @@
-import RestaurantCard from "./RestaurantCard";
-import resList from "../utils/mockData.js";
+import { useEffect, useState } from 'react';
+import RestaurantCard from './RestaurantCard';
+import Shimmer from './Shimmer';
+import mockData from '../utils/mockData'; // Import mock data
 
+const Body = () => {
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
+  const [filteredRestaurant, setFilteredRestaurant] = useState([]);
+  const [searchText, setSearchText] = useState('');
 
-const Body = () =>{
-    return (
-        <div className="body">
-            <div className="filter">
-                <button 
-                className="filter-btn" 
-                onClick={() =>{
-                    console.log("Button clicked");
-                }}>
-                    Top Rated Restaurant  
-                </button>
-            </div>
-            <div className="res-container">
-             {resList.map((restaurant)=> ( 
-             <RestaurantCard key={restaurant.id} resData={restaurant} />
-             ))}
-             
-            
-            </div>
+  console.log('Body rendered');
+
+  useEffect(() => {
+    // Use mock data directly instead of fetching
+    setListOfRestaurants(mockData);
+    setFilteredRestaurant(mockData);
+  }, []);
+
+  return listOfRestaurants.length === 0 ? (
+    <Shimmer />
+  ) : (
+    <div className="body">
+      <div className="filter">
+        <div className="search">
+          <input
+            type="text"
+            placeholder="Search a restaurant you want..."
+            className="searchBox"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          />
+          <button
+            onClick={() => {
+              const filteredRestaurant = listOfRestaurants.filter((res) =>
+                res.data.name.toLowerCase().includes(searchText.toLowerCase())
+              );
+              setFilteredRestaurant(filteredRestaurant);
+            }}
+          >
+            Search
+          </button>
         </div>
-    )
-}
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = listOfRestaurants.filter(
+              (res) => res.data.avgRating > 4
+            );
+            setFilteredRestaurant(filteredList);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
+      <div className="res-container">
+        {filteredRestaurant.map((restaurant) => (
+          <RestaurantCard key={restaurant.data.id} resData={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
 export default Body;
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,22 +1,39 @@
-import {LOGO_URL} from "../utils/constants.js";
+import { LOGO_URL } from '../utils/constants';
+import { useState } from 'react';
 
-const Header = () =>
-    {
-        return (
-            <div className="header">
-                <div className="logo-container">
-                <img className="logo" src={LOGO_URL} />
-                </div>
-                <div className="nav-items">
-                    <ul>
-                        <li>Home</li>
-                        <li>About Us</li>
-                        <li>Contact Us</li>
-                        <li>Cart</li>
-                    </ul>
-                </div>
-            </div>
-        )
-    }
+const Header = () => {
+  //   let btnName = 'Login';
 
-    export default Header;
+  const [btnNameReact, setBtnNameReact] = useState('Login');
+  console.log('header render');
+
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img src={LOGO_URL} alt="App Logo" className="logo" />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+          <button
+            className="loginBtn"
+            onClick={() => {
+              //   btnName = 'Logout';
+              btnNameReact === 'Login'
+                ? setBtnNameReact('Logout')
+                : setBtnNameReact('Login');
+              console.log(btnNameReact);
+            }}
+          >
+            {btnNameReact}
+          </button>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
